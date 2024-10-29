1facef6a19002d3f85142e7f5426e0f6589aac76
DeliaGlez <deliavargas.glez@gmail.com>
2024-10-29 00:03:40 -0700

    A20

---
+++ U4/A20/ProductController.php
@@ -0,0 +1,280 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['accion'])) {
+        $action = $_POST['accion'];
+
+        $productController = new ProductController();
+
+        switch ($action) {
+            case 'store':
+                $name = $_POST['name'];
+                $slug = $_POST['slug'];
+                $description = $_POST['description'];
+                $features = $_POST['features'];
+                $brand_id = $_POST['brand_id'];
+                $categories = $_POST['categories'];
+                $tags = $_POST['tags'];
+                $cover = $_FILES['cover']['tmp_name'];
+
+                $response = $productController->store($name, $slug, $description, $features, $brand_id, $categories, $tags,$cover);
+                
+                break;
+            case 'update':
+                $id = $_POST['product_id']; 
+                $name = $_POST['name'];
+                $slug = $_POST['slug'];
+                $description = $_POST['description'];
+                $features = $_POST['features'];
+                $brand_id = $_POST['brand_id'];
+                $categories = $_POST['categories'];
+                $tags = $_POST['tags'];
+
+                
+                $response = $productController->update($id,$name, $slug, $description, $features, $brand_id, $categories, $tags);
+                break;
+
+            case 'delete':
+                $id = $_POST['delete_product_id']; 
+                $response = $productController->delete($id);
+
+                break;
+            default:
+                echo "Acción desconocida";
+                break;
+        }
+    }
+}
+
+
+class ProductController {
+    private $apiUrl = 'https://crud.jonathansoto.mx/api/products';
+
+    public function getProducts() {
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->apiUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . $token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($response, true); 
+    }
+
+    public function getProduct($slug){
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products/slug/'.$slug,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+    
+        if ($response) {
+            $data = json_decode($response, true);
+            if (isset($data['data'])) {
+                return $data['data'];
+            }
+        }else {
+            return [];
+        }       
+
+       
+
+    }
+    public function getBrands(){
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $token
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $result = json_decode($response, true);
+
+        if (isset($result['data'])) {
+            return $result['data'];
+        }
+
+        return [];
+    }
+    public function store($name, $slug, $description, $features, $brand_id, $categories, $tags,$cover) {
+        
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+    
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $this->apiUrl,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => array(
+            'name' => $name,
+            'slug' => $slug,
+            'description' => $description,
+            'features' => $features,
+            'brand_id' => $brand_id,
+            'cover'=> new CURLFILE($cover),
+            'categories[0]' => $categories,  
+            'tags[0]' => $tags,
+          ),
+          CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $token
+          ),
+        ));
+    
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+
+        $responseData = json_decode($response, true);
+        if (isset($responseData['data'])) {
+            header("Location: index.php?success=ok");
+        } else {
+            header("Location: index.php?error=error");
+        }
+
+        return $response;
+    }
+
+    public function update($id,$name, $slug, $description, $features, $brand_id, $categories, $tags) {
+        
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+    
+        if (!isset($_SESSION['token'])) {
+            echo 'No se encontró el token de autorización.';
+            return [];
+        }
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => json_encode(array(
+                'id' => $id,
+                'name' => $name,
+                'slug' => $slug,
+                'description' => $description,
+                'features' => $features,
+                'brand_id' => $brand_id,
+                'categories[0]' => $categories,  
+                'tags[0]' => $tags,
+            )),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $responseData = json_decode($response, true);
+        if (isset($responseData['data'])) {
+            header("Location: index.php?success=ok");
+        } else {
+            header("Location: index.php?error=error");
+        }
+
+        return $response;
+    }
+    public function delete($id) {
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : '';
+    
+        if (!isset($token)) {
+            echo 'No se encontró el token de autorización.';
+            return [];
+        }
+    
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products/' . intval($id), 
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . $token
+            ),
+        ));
+    
+        $response = curl_exec($curl);
+        curl_close($curl);
+    
+        $responseData = json_decode($response, true);
+    
+        if (isset($responseData['code']) && $responseData['code'] === 2) {
+            header("Location: index.php?success=ok");
+            exit(); 
+        } else {
+            header("Location: index.php?error=error");
+            exit(); 
+        }
+    
+        return $response;
+    }
+    
+    
+}
+
+
+?>
---
+++ U4/A20/details.php
@@ -0,0 +1,279 @@
+<?php
+session_start(); 
+
+if (!isset($_SESSION['token'])) {
+    header('Location: login.php');
+    exit();
+}
+require_once 'ProductController.php';
+if (isset($_GET['slug'])) {
+  $slug = $_GET['slug'];  
+
+  $productController = new ProductController();
+  $product = $productController->getProduct($slug);
+  if (!$product) {
+    header('Location: index.php');
+    exit();
+  }
+  $name = isset($_SESSION['user_name']) ? $_SESSION['user_name'] : 'Usuario'; 
+
+} else {
+  header('Location: index.php');
+  exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row">
+
+      <div
+        class="col-md-2 bg-dark text-white d-flex flex-column d-none d-sm-none d-md-block vh-100 position-sticky sticky-top">
+        <h4 class="p-3">Sidebar</h4>
+        <hr>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link text-white active" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Orders</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Customers</a>
+          </li>
+        </ul>
+
+        <div class="mt-auto p-3">
+          <hr>
+          <div class="dropdown">
+            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+              id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+              <strong>
+                <?= $name; ?>
+              </strong>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+              <li><a class="dropdown-item" href="#">New project...</a></li>
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li>
+                <form action="AuthController.php" method="POST">
+                  <input type="hidden" name="accion" value="logout">
+                  <button class="dropdown-item" type="submit">Sign out</button>
+                </form>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-md-10 p-0">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar scroll</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+              <form class="d-flex ms-auto" action="AuthController.php" method="POST">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                <div class="dropdown ps-3">
+                  <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                    id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <strong>
+                      <?= $name; ?>
+                    </strong>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-dark text-small shadow dropdown-menu-end"
+                    aria-labelledby="dropdownUser1" style="">
+                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                      <form action="AuthController.php" method="POST">
+                        <input type="hidden" name="accion" value="logout">
+                        <button class="dropdown-item" type="submit">Sign out</button>
+                      </form>
+                    </li>
+                  </ul>
+                </div>
+              </form>
+            </div>
+          </div>
+        </nav>
+
+        <div class="container mt-4">
+          <h2>Productos</h2>
+          <div class="row">
+
+            <div class="card">
+              <div class="card-header">
+                <?= $product['brand']['name']?>
+              </div>
+              <div class="card-body">
+                <div class="row">
+
+                  <div id="carouselExampleIndicators" class="carousel slide col-12 col-md-3">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                        class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src=<?=$product['cover'] ?>
+                        class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src=<?=$product['cover'] ?>
+                        class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src=<?=$product['cover'] ?>
+                        class="d-block w-100" alt="...">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+
+                  <div class="col-9">
+                    <h5 class="card-title">
+                      <?=$product['name'] ?>
+                    </h5>
+                    <p class="card-text">$
+                      <?= isset($product['presentations'][0]['price'][0]['amount']) ? htmlspecialchars($product['presentations'][0]['price'][0]['amount']) : 'No disponible'; ?>
+                    </p>
+
+                    <p class="card-text">
+                      <?=  $product['description'] ?>
+                    </p>
+                    <p class="card-text"> <strong>Características:</strong>
+                      <?=  $product['features'] ?>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+
+                </div>
+
+                <div class="row">
+                  <div class="col-12 mt-4">
+
+
+                    <h4>Tags</h4>
+                    <table class="table">
+                      <thead class='table-dark'>
+                        <tr>
+                          <th scope="col">Nombre</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php if (isset($product['tags'])): ?>
+                        <?php foreach ($product['tags'] as $tag): ?>
+                        <tr>
+                          <td>
+                            <?php echo htmlspecialchars($tag['name']); ?>
+                          </td>
+                        </tr>
+                        <?php endforeach; ?>
+                        <?php else: ?>
+                        <tr>
+                          <td colspan="3">No hay tags disponibles.</td>
+                        </tr>
+                        <?php endif; ?>
+                      </tbody>
+                    </table>
+
+                    <h4>Categorías</h4>
+                    <table class="table">
+                      <thead class='table-dark'>
+                        <tr>
+                          <th scope="col">Nombre</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php if (isset($product['categories'])): ?>
+                        <?php foreach ($product['categories'] as $category): ?>
+                        <tr>
+                          <td>
+                            <?php echo htmlspecialchars($category['name']); ?>
+                          </td>
+                        </tr>
+                        <?php endforeach; ?>
+                        <?php else: ?>
+                        <tr>
+                          <td colspan="3">No hay categorías disponibles.</td>
+                        </tr>
+                        <?php endif; ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+
+              </div>
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
