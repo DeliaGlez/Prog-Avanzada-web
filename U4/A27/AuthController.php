a824b7a4f8c9228d9155850a40541fb1a13bfb94
DeliaGlez <deliavargas.glez@gmail.com>
2024-10-30 02:45:08 -0700

    A27

---
+++ U4/A27/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}   
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['accion'])) {
+        $action = $_POST['accion'];
+
+        $authController = new AuthController();
+        
+
+        switch ($action) {
+            case 'login':
+                $email = $_POST['correo'];
+                $password = $_POST['contrasenia'];
+                $authController->login($email, $password);
+                break;
+
+            case 'logout':
+                $authController->logout();
+                break;
+
+            default:
+                echo "Accion desconocida";
+                break;
+        }
+    }
+}
+
+class AuthController
+{
+    
+    public function login($email = null, $password = null)
+    {
+        
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+                'email' => $email,       
+                'password' => $password  
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $data = json_decode($response, true);
+
+        if (isset($data['data']['token'])) {
+            $_SESSION['token'] = $data['data']['token'];
+            $_SESSION['user_id'] = $data['data']['id']; 
+            $_SESSION['user_name'] = $data['data']['name']; 
+            $_SESSION['user_email'] = $data['data']['email']; 
+            if (!isset($_SESSION['global_token'])) {
+                 $_SESSION['global_token'] = bin2hex(random_bytes(32));
+            }
+            header('Location: home');
+        } else {
+            header('Location: login');
+        }
+    }
+
+    public function logout()
+    {
+        session_unset();
+        session_destroy();
+        header('Location: login');
+        
+    }
+
+    function getGlobalToken() {
+        if (!isset($_SESSION['global_token'])) {
+            $_SESSION['global_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['global_token'];
+    }
+}
+ 
+
+?>
